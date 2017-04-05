1edab8cfd65c13b725c849646b460e23a4a5fc30
louis-langholtz <lou_langholtz@me.com>
2017-04-05 11:06:34 -0600

    Fixes copyright

--- include/boost/units/systems/si/inverse_mass.hpp
+++ include/boost/units/systems/si/inverse_mass.hpp
@@ -1,8 +1,7 @@
 // Boost.Units - A C++ library for zero-overhead dimensional analysis and
 // unit/quantity manipulation and conversion
 //
-// Copyright (C) 2003-2008 Matthias Christian Schabel
-// Copyright (C) 2008 Steven Watanabe
+// Copyright (C) 2017 Louis Langholtz
 //
 // Distributed under the Boost Software License, Version 1.0. (See
 // accompanying file LICENSE_1_0.txt or copy at
